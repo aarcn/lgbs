d63a7f65cc29bf27692a53bbe16f0cdfd1398652
Aaron <119821072+aarcn@users.noreply.github.com>
2025-02-27 05:45:20 -0600

    accommodated main.py changes

--- lb.py
+++ lb.py
@@ -3,12 +3,12 @@ import time
 from random import uniform
 
 
-def print_loading_bar(total, prefix='', suffix='', length=50, fill='█'):
-    for single_loading_bar in range(total + 1):
-        percent = f"{100 * (single_loading_bar / total):.1f}"
-        filled_length = int(length * single_loading_bar // total)
-        bar = fill * filled_length + '-' * (length - filled_length)
+def loading_bar(total, prefix='', suffix='', length=50, fill='█'):
+    for bar in range(total + 1):
+        percent = f"{100 * (bar / total):.1f}"
+        fill_length = int(length * bar // total)
+        bar = fill * fill_length + '-' * (length - fill_length)
         sys.stdout.write(f'\r{prefix} |{bar}| {percent}% {suffix}')
         sys.stdout.flush()
-        time.sleep(uniform(0.0, 0.06))  # evaluate how long u wanna stall
+        time.sleep(uniform(0.0, 0.06))  # how long u wanna stall
     sys.stdout.write("\n")
